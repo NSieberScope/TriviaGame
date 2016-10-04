c596e96296579d6f13b450a09d56bf65342b4d14
Nick Sieber <Nick@nicks-imac.local>
2016-10-04 16:19:22 -0700

    First Commit of Trivia Project

--- Trivia Game/ViewController.swift
+++ Trivia Game/ViewController.swift
@@ -7,11 +7,30 @@
 //
 
 import UIKit
+import GameKit
+import AudioToolbox
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Question: UILabel!
+    @IBOutlet weak var AButton: UIButton!
+    @IBOutlet weak var BButton: UIButton!
+    @IBOutlet weak var CButton: UIButton!
+    @IBOutlet weak var DButton: UIButton!
+    @IBOutlet weak var PlayAgain: UIButton!
+    @IBOutlet weak var PlayButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        AButton.isHidden = true
+        BButton.isHidden = true
+        CButton.isHidden = true
+        DButton.isHidden = true
+        PlayAgain.isHidden = true
+        Question.text = "Welcome to the trivia challenge!"
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +39,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func playButton() {
+        
+        PlayButton.isHidden = true
+        AButton.isHidden = false
+        BButton.isHidden = false
+        CButton.isHidden = false
+        DButton.isHidden = false
+        
+    }
+    
+    
 
 }
 
